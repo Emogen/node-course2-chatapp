30a7c0d97aa87a144ce68f336f01c584b61fd50f
Singgih Anggana <phoenixgame2@gmail.com>
2018-11-02 10:21:38 +0700

    First Init

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('./config/config');
+
+const path = require('path');
+const express = require('express');
+
+var publicPath = path.join(__dirname,'../public');
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+const port = process.env.PORT;
+
+app.listen(port,()=>{
+	console.log(`Started on port ${port}`);
+})
+
+module.exports = {app};
+
+
