4708709edc5ef309861e82998ef955f2927d4964
Singgih Anggana <phoenixgame2@gmail.com>
2018-11-05 12:31:22 +0700

    Adding styles to the app

--- public/js/index.js
+++ public/js/index.js
@@ -30,14 +30,16 @@ socket.on('newLocationMessage',function(message){
 		$('#messages').append(li);
 });
 
+var messageTextBox = $('[name=message]')
+
 $('#message-form').on('submit',function(e){
 	e.preventDefault();
 
 	socket.emit('createMessage',{
 		from: 'User',
-		text: $('[name=message]').val()
+		text: messageTextBox.val()
 	}, function(data){
-		console.log('Got it',data);
+		messageTextBox.val('');
 	});
 });
 
@@ -47,12 +49,15 @@ locationButton.on('click',function(e){
 	if(!navigator.geolocation){
 		return alert('Geolocation not supported by your browser.');
 	}
+	locationButton.attr('disabled','disabled').text('Sending Location....');
 	navigator.geolocation.getCurrentPosition(function(position){
 		socket.emit('createLocationMessage',{
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
+		locationButton.removeAttr('disabled').text('Send Location');
 	},function(){
 		alert('Unable to fetch location');
+		locationButton.removeAttr('disabled').text('Send Location');
 	});
 });
