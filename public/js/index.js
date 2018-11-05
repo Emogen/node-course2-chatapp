9dbdc8c56e46f9bdc275beefe09351206b468175
Singgih Anggana <phoenixgame2@gmail.com>
2018-11-05 10:35:52 +0700

    Add or messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -12,4 +12,22 @@ socket.on('disconnect',function() {
 
 socket.on('newMessage',function(message){
 	console.log('got new message ',message);
+
+	var li = $('<li></li>');
+	li.text(`${message.from} : ${message.text}`);
+
+	$('#messages').append(li);
+});
+
+
+
+$('#message-form').on('submit',function(e){
+	e.preventDefault();
+
+	socket.emit('createMessage',{
+		from: 'User',
+		text: $('[name=message]').val()
+	}, function(data){
+		console.log('Got it',data);
+	});
 });
