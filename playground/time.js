5239bddb28e696b6658a23d028f0dbd3cf9954ac
Singgih Anggana <phoenixgame2@gmail.com>
2018-11-05 14:21:37 +0700

    Add mustachejs for message templates

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+var moment = require('moment');
+
+
+// var date = new Date();
+//
+// console.log(date.getMonth())
+
+var date = moment();
+
+console.log(date.format('DD MMM YYYY'));
--- public/js/index.js
+++ public/js/index.js
@@ -11,23 +11,29 @@ socket.on('disconnect',function() {
 
 
 socket.on('newMessage',function(message){
-	console.log('got new message ',message);
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = $('#message-template').html();
+	var html = Mustache.render(template,{
+		text: message.text,
+		from: message.from,
+		createdAt: moment(message.createdAt).format('h:mm a')
+	});
 
-	var li = $('<li></li>');
-	li.text(`${message.from} : ${message.text}`);
+	$('#messages').append(html);
 
-	$('#messages').append(li);
+
 });
 
 socket.on('newLocationMessage',function(message){
-		var li = $('<li></li>');
-		var a =$('<a target="_blank">My Current Location</a>');
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = $('#location-message-template').html();
+	var html = Mustache.render(template,{
+		url: message.url,
+		from: message.from,
+		createdAt: moment(message.createdAt).format('h:mm a')
+	});
 
-		li.text(`${message.from}: `);
-		a.attr('href',message.url);
-
-		li.append(a);
-		$('#messages').append(li);
+	$('#messages').append(html);
 });
 
 var messageTextBox = $('[name=message]')
